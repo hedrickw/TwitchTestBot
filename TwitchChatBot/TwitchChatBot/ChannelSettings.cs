d96ce577608be334151b1ec636bcb17536fe934a
RallyCapCoder <rallycapcoding@gmail.com>
2017-02-16 19:51:05 -0500

    trying to get auth to work

---
+++ TwitchChatBot/TwitchChatBot/ChannelSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchChatBot
+{
+    public class ChannelSettings
+    {
+        public string Status { get; set; }
+        public string Game { get; set; }
+        public string Delay { get; set; }
+        public bool Channel_Feed_Enabled { get; set; }
+
+        public ChannelSettings(string status,string game,string delay,bool channel_feed_enabled)
+        {
+            Status = status;
+            Game = game;
+            Delay = delay;
+            Channel_Feed_Enabled = channel_feed_enabled;
+        }
+    }
+}
--- TwitchChatBot/TwitchChatBot/Program.cs
+++ TwitchChatBot/TwitchChatBot/Program.cs
@@ -13,7 +13,15 @@ namespace TwitchChatBot
     {
         public static void Main(string[] args)
         {
-            string urlToRequest = "https://api.twitch.tv/kraken";
+            //Console.WriteLine("Enter Channel Name you want information on");
+            //var channelName = Console.ReadLine();
+            UpdatePersonalTwitchChannelInfo("", "", "");
+            //GetTwitchChannelInfo(channelName);
+        }
+
+        public static void GetTwitchChannelInfo(string channelName)
+        {
+            string urlToRequest = "https://api.twitch.tv/kraken/channels" + "/" + $"{channelName}";
 
             var request = WebRequest.Create(urlToRequest);
             request.Headers.Add("Client-ID: lyf7vi5gq1sh3g1j4oovlm4jfe77sx");
@@ -21,9 +29,51 @@ namespace TwitchChatBot
 
             using (var reader = new StreamReader(stuff.GetResponseStream()))
             {
-                var moarStuff = JsonConvert.DeserializeObject(reader.ReadToEnd());
+                var moarStuff = JsonConvert.DeserializeObject(reader.ReadToEnd()).ToString();
+
+                Console.WriteLine(moarStuff);
             }
-              //  var response = JsonConvert.DeserializeObject(stuff.ToString());
+
+            Console.ReadLine();
+        }
+
+        //public static AuthenticateStuff()
+        //{
+        //    string urlToSend = @"https://api.twitch.tv/kraken/oauth2/authorize?response_type=token&client_id=lyf7vi5gq1sh3g1j4oovlm4jfe77sx&redirect_uri=http://localhost/TestingThis&scope=channel_editor&state=w51d28xtnez1ydui9sa6fv2jot4sq2";
+        //}
+
+        public static void UpdatePersonalTwitchChannelInfo(string game, string status, string delay)
+        {
+            string urlToRequest = "https://api.twitch.tv/kraken/channels" + "/" + $"25666162";
+
+            var request = (HttpWebRequest)WebRequest.Create(urlToRequest);
+            
+            request.Headers.Add("Client-ID: lyf7vi5gq1sh3g1j4oovlm4jfe77sx");
+            request.Accept = "application/vnd.twitchtv.v3+json";
+            request.Headers.Add("Authorization: OAuth pbiv3impos6muep48om5apt079k6it");
+            //request.ContentType = "application/json";
+            request.Method = "PUT";
+            var channelSettings = new ChannelSettings("Updating channel through code", "Visual Studio 2017", "", true);
+
+            var jsonData = JsonConvert.SerializeObject(channelSettings);
+
+            using (var writer = new StreamWriter(request.GetRequestStream()))
+            {
+                //writer.Write("{channel: {status: The Finalest of Fantasies, game: Final Fantasy XV, channel_feed_enabled: true}}");
+                writer.Write("channel[status] = The + Finalest + of + Fantasies & channel[game] = Final + Fantasy + XV");
+            }
+
+            
+            var stuff = request.GetResponse();
+
+            using (var reader = new StreamReader(stuff.GetResponseStream()))
+            {
+                var moarStuff = JsonConvert.DeserializeObject(reader.ReadToEnd()).ToString();
+
+                Console.WriteLine(moarStuff);
+            }
+
+            Console.ReadLine();
         }
     }
 }
